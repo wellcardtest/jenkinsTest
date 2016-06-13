3ea90c0cb1deaee0921c1e44e09f98357676e754
Wellington Paixao <wellington.paixao@montreal.com.br>
2016-06-13 18:27:56 -0300

    novo commit

---
+++ src/main/java/br/gov/mds/jenkinsTest/App.java
@@ -0,0 +1,13 @@
+package br.gov.mds.jenkinsTest;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/br/gov/mds/regressao/IntegracaoJenkins.java
@@ -0,0 +1,32 @@
+package br.gov.mds.regressao;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+public class IntegracaoJenkins {
+
+	WebDriver driver;
+	
+	@BeforeTest
+	public void setup() {
+		driver = new FirefoxDriver();
+		
+	}
+	
+	@Test
+	public void testeTitulo(){
+		driver.get("http://www.google.com.br");
+		Assert.assertTrue(driver.getTitle().contains("Google"));
+		
+	}
+	
+	@AfterTest
+	public void tearDown(){
+		driver.close();
+		driver.quit();
+	}
+}
